08fc002a80bf211ad148a7a0baf55c683e41103b
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-06-21 23:07:59 +0200

    prettier run

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import { quizData, loadQuizProgress } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
 const loadApp = () => {
-  loadQuizProgress();  //  Load saved progress from localStorage
-  initWelcomePage();   //  Show welcome/start screen
+  loadQuizProgress(); //  Load saved progress from localStorage
+  initWelcomePage(); //  Show welcome/start screen
 };
 
 window.addEventListener('load', loadApp);
